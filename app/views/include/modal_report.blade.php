0c57e3608b4c848f217f59d1770e2de322571c78
Robert Reimi <robert.reimi@gmail.com>
2013-07-27 20:43:26 -0430

    Updated report form markup

--- app/views/include/modal_report.blade.php
+++ app/views/include/modal_report.blade.php
@@ -3,12 +3,10 @@
         <a href="javascript:Mercatino.reportForm.hide()" class="close">&times;</a>
         <h3>{{Lang::get('content.report_it')}}</h3>
     </div>
-    <div class="row-fluid">
-        <span class="offset1 span11">{{Lang::get('content.report_instructions')}}</span>
-    </div>
 
-    <div class="row-fluid">
-        <textarea id="report_comment_txt" name="report_comment" class="offset1 span10" rows="8"></textarea>
+    <div class="modal-body">
+        <span class="divider-vertical" style="margin-bottom:1em;">{{Lang::get('content.report_instructions')}}</span>
+        <textarea id="report_comment_txt" class="input-block-level" name="report_comment" rows="8"></textarea>
     </div>
     <div class='modal-footer'>
         <a href="javascript:Mercatino.reportForm.send()" class="btn btn-warning">{{Lang::get('content.report_it')}}</a>
