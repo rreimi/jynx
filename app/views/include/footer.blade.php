b109cfc8c71148846d3ce124c7bf4e8afde7d8f5
andrealebrun <abracadabra2>
2013-07-28 23:38:20 -0430

    * Added footer.

--- app/views/include/footer.blade.php
+++ app/views/include/footer.blade.php
@@ -1,1 +1,8 @@
-<p>&copy; 2013 MercatinOnline.</p>
+<div class="footer" id="footer">
+    <div class="copyright">
+        &copy; Copyright 2013 - Mercatino
+        <div class="androb">
+            Desarrollado por <a href="http://www.androb.com" target="_blank">ANDROB</a>
+        </div>
+    </div>
+</div>
