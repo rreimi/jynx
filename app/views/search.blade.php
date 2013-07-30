67709482a0978b52211f4a94a1ced35b819e0651
Robert Reimi <robert.reimi@gmail.com>
2013-07-30 18:00:58 -0430

    Fix issue with lists key+1 issue

--- app/views/search.blade.php
+++ app/views/search.blade.php
@@ -13,13 +13,26 @@
 
     @foreach ($publications as $key => $pub)
         @if ($key % 3 == 0)
-        <div class="row-fluid"><!-- /begin row-fluid -->
+        <div class="row-fluid">
         @endif
-            <div class="span4 pub-thumb"><!--/begin pub-thumb-->
-                A
-            </div><!--/end pub-thumb-->
+            <div class="span4 pub-thumb">
+                <div class="put-info-box">
+                    @if (isset($pub->images[0]))
+                    <a href="{{ URL::to('publicacion/detalle/' . $pub->id)}}">
+                        <img class="pub-img-small"  src="{{ Image::path('/uploads/pub/' . $pub->id . '/' . $pub->images[0]->image_url, 'resize', $thumbSize['width'], $thumbSize['height'])  }}" alt="{{ $pub->title }}"/>
+                    </a>
+                    @endif
+                    <div class="pub-info-desc">
+                        <a href="{{ URL::to('publicacion/detalle/' . $pub->id)}}">
+                            <h2 class="pub-title">{{ $pub->title }}</h2>
+                        </a>
+                        <span class="pub-seller">{{Lang::get('content.sell_by')}}: {{ $pub->publisher->seller_name }}</span>
+                        <!--                <p class="pub-short-desc"> $pub->short_description </p>-->
+                    </div>
+                </div>
+            </div><!--/span-->
         @if (((($key+1)%3) == 0) || ($key+1 == count($publications)))
-        </div><!-- /end row-fluid -->
+        </div>
         @endif
     @endforeach
     {{ $publications->appends(Input::except('page'))->links() }}
