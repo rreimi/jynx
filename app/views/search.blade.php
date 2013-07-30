be22badd6fac233bc50170e2702dacec08f467c8
Robert Reimi <robert.reimi@gmail.com>
2013-07-30 17:58:35 -0430

    Fix issue with bad markup in paginate carousels and lists (key+1 issue)

--- app/views/search.blade.php
+++ app/views/search.blade.php
@@ -13,26 +13,13 @@
 
     @foreach ($publications as $key => $pub)
         @if ($key % 3 == 0)
-        <div class="row-fluid">
+        <div class="row-fluid"><!-- /begin row-fluid -->
         @endif
-            <div class="span4 pub-thumb">
-                <div class="put-info-box">
-                    @if (isset($pub->images[0]))
-                    <a href="{{ URL::to('publicacion/detalle/' . $pub->id)}}">
-                        <img class="pub-img-small"  src="{{ Image::path('/uploads/pub/' . $pub->id . '/' . $pub->images[0]->image_url, 'resize', $thumbSize['width'], $thumbSize['height'])  }}" alt="{{ $pub->title }}"/>
-                    </a>
-                    @endif
-                    <div class="pub-info-desc">
-                        <a href="{{ URL::to('publicacion/detalle/' . $pub->id)}}">
-                            <h2 class="pub-title">{{ $pub->title }}</h2>
-                        </a>
-                        <span class="pub-seller">{{Lang::get('content.sell_by')}}: {{ $pub->publisher->seller_name }}</span>
-                        <!--                <p class="pub-short-desc"> $pub->short_description </p>-->
-                    </div>
-                </div>
-            </div><!--/span-->
-        @if ((($key+1)%3) == 0)
-        </div>
+            <div class="span4 pub-thumb"><!--/begin pub-thumb-->
+                A
+            </div><!--/end pub-thumb-->
+        @if (((($key+1)%3) == 0) || ($key+1 == count($publications)))
+        </div><!-- /end row-fluid -->
         @endif
     @endforeach
     {{ $publications->appends(Input::except('page'))->links() }}
