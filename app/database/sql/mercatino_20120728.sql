f47d7c3a08086e946d0dad5b79818b46cf371e16
andrealebrun <abracadabra2>
2013-08-05 18:10:10 -0430

    * Changed publisher_id to user_id in ratings.

--- app/database/sql/mercatino_20120728.sql
+++ app/database/sql/mercatino_20120728.sql
@@ -252,23 +252,23 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `mercatino`.`publications_ratings` (
   `id` INT NOT NULL AUTO_INCREMENT ,
-  `publisher_id` INT NOT NULL ,
+  `user_id` INT NOT NULL ,
   `publication_id` INT NOT NULL ,
   `vote` INT NOT NULL ,
   `comment` VARCHAR(300) NULL ,
   `created_at` DATETIME NOT NULL ,
   `updated_at` DATETIME NOT NULL ,
   PRIMARY KEY (`id`) ,
-  INDEX `fk_publication_raitings_publisher_idx` (`publisher_id` ASC) ,
+  INDEX `fk_publication_raitings_user_idx` (`user_id` ASC) ,
   INDEX `fk_publication_raitings_publication_idx` (`publication_id` ASC) ,
   CONSTRAINT `fk_publications_raitings_publications`
     FOREIGN KEY (`publication_id` )
     REFERENCES `mercatino`.`publications` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_publications_raitings_publishers`
-    FOREIGN KEY (`publisher_id` )
-    REFERENCES `mercatino`.`publishers` (`id` )
+  CONSTRAINT `fk_publications_raitings_users`
+    FOREIGN KEY (`user_id` )
+    REFERENCES `mercatino`.`users` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
