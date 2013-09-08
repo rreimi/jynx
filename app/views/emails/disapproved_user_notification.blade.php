2aa8d3deaacd7a437666ae99eb81bdd390783bb9
josemtroconis <jm010387>
2013-09-08 15:26:55 -0430

    * Fixed error in email template.

--- app/views/emails/disapproved_user_notification.blade.php
+++ app/views/emails/disapproved_user_notification.blade.php
@@ -3,7 +3,7 @@ Estimado {{ $userName }},<br/>
 Hemos revisado nuestra base de datos y no tenemos registro que {{ $sellerName }}, RIF {{ $letterRifCi }}-{{ $rifCi }},
 sea socia de la Cámara de Comercio Venezolana-Italiana.<br/>
 <br/>
-Si ha sido un error de nuestra parte, por favor escríbenos a {{ Lang::get('content.delete_advertising_invalid') }}.<br/>
+Si ha sido un error de nuestra parte, por favor escríbenos a {{ Config::get('emails/addresses.email_contactus') }}.<br/>
 <br/>
 Si en efecto no formas parte de CAVENIT y deseas mayor información sobre cómo convertirte en afiliado, escríbenos a
 {{ Config::get('emails/addresses.email_contactus') }}.<br/>
