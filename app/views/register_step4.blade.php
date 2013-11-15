d931f4eab154577bc6b48dcb0b3baee94327d320
josemtroconis <jm010387>
2013-11-15 16:14:51 -0430

    * Adjusted style of step 4 of register process.

--- app/views/register_step4.blade.php
+++ app/views/register_step4.blade.php
@@ -4,13 +4,12 @@
     <div class="row-fluid">
         <div class="span1"></div>
         <div class="span10">
-            <h3 id="myModalLabel">{{ Lang::get('content.publisher_success_title') }}</h3>
-            <div class="row-fluid">
-                <p>{{ Lang::get('content.publisher_success_message') }}</p>
-                <a class="btn btn-large btn-warning publisher-info" href="{{ URL::to('/') }}">{{ Lang::get('content.publisher_success_accept') }}</a>
+            <div class="big-form register-success">
+                <h3>{{ Lang::get('content.publisher_success_title') }}</h3>
+                <p class="text">{{ Lang::get('content.publisher_success_message') }}</p>
+                <a class="btn btn-large btn-warning" href="{{ URL::to('/') }}">{{ Lang::get('content.publisher_success_accept') }}</a>
             </div>
         </div>
-        <div class="span1"></div>
     </div>
 </div>
 @stop
