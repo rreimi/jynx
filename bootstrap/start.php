2baa2db0a16885c1f9cb5411e80628bc44a808c9
Robert Reimi <robert.reimi@gmail.com>
2014-02-16 22:29:46 +0000

    Added enviroment configuration

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -28,7 +28,7 @@ $app->redirectIfTrailingSlash();
 
 $env = $app->detectEnvironment(array(
 	'dev' => array('dev.jynx.com', 'andrea-PC', 'abnet', 'fergie', 'jose-laptop'),
-    'pro' => array('www.jynx.com')
+    'test' => array('prueba.tumercato.com', 'mercatino.androb.me')
 ));
 
 /*
