f5d176260ba43849cb5156a58f863d7aa3b806a2
Robert Reimi <robert.reimi@gmail.com>
2014-02-24 01:26:38 +0000

    Ready new requirement feature-1, Make seller links points to all seller products

--- app/views/include/publication_box.blade.php
+++ app/views/include/publication_box.blade.php
@@ -14,7 +14,7 @@
         <a href="{{ URL::to('publicacion/detalle/' . $pub->id)}}">
             <h2 class="pub-title" title="{{ $pub->title }}">{{ TextHelper::truncate($pub->title, 45) }}</h2>
         </a>
-        <span class="pub-seller masterTooltip" title="{{ $pub->seller_name }}">{{Lang::get('content.sell_by')}}: {{ $pub->seller_name }}</span>
+        <span class="pub-seller masterTooltip" title="{{ $pub->seller_name }}">{{Lang::get('content.sell_by')}}: <a href="{{ URL::to('search?seller=' . $pub->publisher_id)}}"> {{ $pub->seller_name }} </a></span>
     </div>
 </div>
 
