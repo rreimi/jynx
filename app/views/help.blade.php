7d4a7da0aa5747e73bce82ff0d0b6b91ab05a9d5
andrea <andrealebrun@gmail.com>
2014-04-27 19:46:56 +0100

    * Added FAQ.

--- app/views/help.blade.php
+++ app/views/help.blade.php
@@ -2,6 +2,8 @@
 @parent
 @stop
 
+<a name="pagetop" ></a>
+
 @section('content')
 
 <div class="row-fluid">
@@ -9,12 +11,344 @@
     <h1 class="about-us-title">{{Lang::get('content.help')}}</h1>
 
     <div class="help">
-        <p>Próximamente...</p>
+
+        <h2>{{Lang::get('content.faq_subtitle1')}}</h2>
+
+        <ol>
+            <a href="#1_1"><li>{{Lang::get('content.faq1_1')}}</li></a>
+            <a href="#1_2"><li>{{Lang::get('content.faq1_2')}}</li></a>
+            <a href="#1_3"><li>{{Lang::get('content.faq1_3')}}</li></a>
+            <a href="#1_4"><li>{{Lang::get('content.faq1_4')}}</li></a>
+            <a href="#1_5"><li>{{Lang::get('content.faq1_5')}}</li></a>
+            <a href="#1_6"><li>{{Lang::get('content.faq1_6')}}</li></a>
+            <a href="#1_7"><li>{{Lang::get('content.faq1_7')}}</li></a>
+            <a href="#1_8"><li>{{Lang::get('content.faq1_8')}}</li></a>
+            <a href="#1_9"><li>{{Lang::get('content.faq1_9')}}</li></a>
+            <a href="#1_10"><li>{{Lang::get('content.faq1_10')}}</li></a>
+            <a href="#1_11"><li>{{Lang::get('content.faq1_11')}}</li></a>
+            <a href="#1_12"><li>{{Lang::get('content.faq1_12')}}</li></a>
+            <a href="#1_13"><li>{{Lang::get('content.faq1_13')}}</li></a>
+        </ol>
+
+        <h2>{{Lang::get('content.faq_subtitle2')}}</h2>
+
+        <ol>
+            <a href="#2_1"><li>{{Lang::get('content.faq2_1')}}</li></a>
+            <a href="#2_2"><li>{{Lang::get('content.faq2_2')}}</li></a>
+            <a href="#2_3"><li>{{Lang::get('content.faq2_3')}}</li></a>
+            <a href="#2_4"><li>{{Lang::get('content.faq2_4')}}</li></a>
+            <a href="#2_5"><li>{{Lang::get('content.faq2_5')}}</li></a>
+            <a href="#2_6"><li>{{Lang::get('content.faq2_6')}}</li></a>
+            <a href="#2_7"><li>{{Lang::get('content.faq2_7')}}</li></a>
+            <a href="#2_8"><li>{{Lang::get('content.faq2_8')}}</li></a>
+            <a href="#2_9"><li>{{Lang::get('content.faq2_9')}}</li></a>
+            <a href="#2_10"><li>{{Lang::get('content.faq2_10')}}</li></a>
+            <a href="#2_11"><li>{{Lang::get('content.faq2_11')}}</li></a>
+            <a href="#2_12"><li>{{Lang::get('content.faq2_12')}}</li></a>
+            <a href="#2_13"><li>{{Lang::get('content.faq2_13')}}</li></a>
+            <a href="#2_14"><li>{{Lang::get('content.faq2_14')}}</li></a>
+            <a href="#2_15"><li>{{Lang::get('content.faq2_15')}}</li></a>
+            <a href="#2_16"><li>{{Lang::get('content.faq2_16')}}</li></a>
+            <a href="#2_17"><li>{{Lang::get('content.faq2_17')}}</li></a>
+            <a href="#2_18"><li>{{Lang::get('content.faq2_18')}}</li></a>
+            <a href="#2_19"><li>{{Lang::get('content.faq2_19')}}</li></a>
+            <a href="#2_20"><li>{{Lang::get('content.faq2_20')}}</li></a>
+            <a href="#2_21"><li>{{Lang::get('content.faq2_21')}}</li></a>
+            <a href="#2_22"><li>{{Lang::get('content.faq2_22')}}</li></a>
+            <a href="#2_23"><li>{{Lang::get('content.faq2_23')}}</li></a>
+            <a href="#2_24"><li>{{Lang::get('content.faq2_24')}}</li></a>
+            <a href="#2_25"><li>{{Lang::get('content.faq2_25')}}</li></a>
+            <a href="#2_26"><li>{{Lang::get('content.faq2_26')}}</li></a>
+            <a href="#2_27"><li>{{Lang::get('content.faq2_27')}}</li></a>
+            <a href="#2_28"><li>{{Lang::get('content.faq2_28')}}</li></a>
+            <a href="#2_29"><li>{{Lang::get('content.faq2_29')}}</li></a>
+            <a href="#2_30"><li>{{Lang::get('content.faq2_30')}}</li></a>
+            <a href="#2_31"><li>{{Lang::get('content.faq2_31')}}</li></a>
+            <a href="#2_32"><li>{{Lang::get('content.faq2_32')}}</li></a>
+            <a href="#2_33"><li>{{Lang::get('content.faq2_33')}}</li></a>
+            <a href="#2_34"><li>{{Lang::get('content.faq2_34')}}</li></a>
+            <a href="#2_35"><li>{{Lang::get('content.faq2_35')}}</li></a>
+            <a href="#2_36"><li>{{Lang::get('content.faq2_36')}}</li></a>
+            <a href="#2_37"><li>{{Lang::get('content.faq2_37')}}</li></a>
+            <a href="#2_38"><li>{{Lang::get('content.faq2_38')}}</li></a>
+            <a href="#2_39"><li>{{Lang::get('content.faq2_39')}}</li></a>
+            <a href="#2_40"><li>{{Lang::get('content.faq2_40')}}</li></a>
+            <a href="#2_41"><li>{{Lang::get('content.faq2_41')}}</li></a>
+            <a href="#2_42"><li>{{Lang::get('content.faq2_42')}}</li></a>
+            <a href="#2_43"><li>{{Lang::get('content.faq2_43')}}</li></a>
+            <a href="#2_44"><li>{{Lang::get('content.faq2_44')}}</li></a>
+        </ol>
+
+        <hr/>
+
+        <h2>{{Lang::get('content.faq_subtitle1')}}</h2>
+
+        <dl class="faq">
+            <ol>
+                <li>
+                    <dt id="1_1">{{Lang::get('content.faq1_1')}}</dt>
+                    <dd>{{Lang::get('content.ans1_1')}}</dd>
+                </li>
+                <li>
+                    <dt id="1_2">{{Lang::get('content.faq1_2')}}</dt>
+                    <dd>{{Lang::get('content.ans1_2')}}</dd>
+                </li>
+                <li>
+                    <dt id="1_3">{{Lang::get('content.faq1_3')}}</dt>
+                    <dd>{{Lang::get('content.ans1_3')}}</dd>
+                </li>
+                <li>
+                    <dt id="1_4">{{Lang::get('content.faq1_4')}}</dt>
+                    <dd>{{Lang::get('content.ans1_4')}}</dd>
+                </li>
+                <li>
+                    <dt id="1_5">{{Lang::get('content.faq1_5')}}</dt>
+                    <dd>{{Lang::get('content.ans1_5')}}</dd>
+                </li>
+                <li>
+                    <dt id="1_6">{{Lang::get('content.faq1_6')}}</dt>
+                    <dd>{{Lang::get('content.ans1_6')}}</dd>
+                </li>
+                <li>
+                    <dt id="1_7">{{Lang::get('content.faq1_7')}}</dt>
+                    <dd>{{Lang::get('content.ans1_7')}}</dd>
+                </li>
+                <li>
+                    <dt id="1_8">{{Lang::get('content.faq1_8')}}</dt>
+                    <dd>{{Lang::get('content.ans1_8')}}</dd>
+                </li>
+                <li>
+                    <dt id="1_9">{{Lang::get('content.faq1_9')}}</dt>
+                    <dd>{{Lang::get('content.ans1_9')}}</dd>
+                </li>
+                <li>
+                    <dt id="1_10">{{Lang::get('content.faq1_10')}}</dt>
+                    <dd>{{Lang::get('content.ans1_10')}}</dd>
+                </li>
+                <li>
+                    <dt id="1_11">{{Lang::get('content.faq1_11')}}</dt>
+                    <dd>{{Lang::get('content.ans1_11')}}</dd>
+                </li>
+                <li>
+                    <dt id="1_12">{{Lang::get('content.faq1_12')}}</dt>
+                    <dd>{{Lang::get('content.ans1_12')}}</dd>
+                </li>
+                <li>
+                    <dt id="1_13">{{Lang::get('content.faq1_13')}}</dt>
+                    <dd>{{Lang::get('content.ans1_13')}}</dd>
+                </li>
+            </ol>
+        </dl>
+
+        <h2>{{Lang::get('content.faq_subtitle2')}}</h2>
+
+        <dl class="faq">
+            <ol>
+                <li>
+                    <dt id="2_1">{{Lang::get('content.faq2_1')}}</dt>
+                    <dd>{{Lang::get('content.ans2_1')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_2">{{Lang::get('content.faq2_2')}}</dt>
+                    <dd>{{Lang::get('content.ans2_2')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_3">{{Lang::get('content.faq2_3')}}</dt>
+                    <dd>{{Lang::get('content.ans2_3')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_4">{{Lang::get('content.faq2_4')}}</dt>
+                    <dd>{{Lang::get('content.ans2_4')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_5">{{Lang::get('content.faq2_5')}}</dt>
+                    <dd>{{Lang::get('content.ans2_5')}}
+                        @if (!is_null(Auth::user()) && Auth::user()->isBasic())
+                            {{Lang::get('content.ans2_5_1')}}
+                        @endif
+                    </dd>
+                </li>
+                <li>
+                    <dt id="2_6">{{Lang::get('content.faq2_6')}}</dt>
+                    <dd>{{Lang::get('content.ans2_6')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_7">{{Lang::get('content.faq2_7')}}</dt>
+                    <dd>{{Lang::get('content.ans2_7')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_8">{{Lang::get('content.faq2_8')}}</dt>
+                    <dd>{{Lang::get('content.ans2_8')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_9">{{Lang::get('content.faq2_9')}}</dt>
+                    <dd>{{Lang::get('content.ans2_9')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_10">{{Lang::get('content.faq2_10')}}</dt>
+                    <dd>{{Lang::get('content.ans2_10')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_11">{{Lang::get('content.faq2_11')}}</dt>
+                    <dd>{{Lang::get('content.ans2_11')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_12">{{Lang::get('content.faq2_12')}}</dt>
+                    <dd>{{Lang::get('content.ans2_12')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_13">{{Lang::get('content.faq2_13')}}</dt>
+                    <dd>{{Lang::get('content.ans2_13')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_14">{{Lang::get('content.faq2_14')}}</dt>
+                    <dd>{{Lang::get('content.ans2_14')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_15">{{Lang::get('content.faq2_15')}}</dt>
+                    <dd>{{Lang::get('content.ans2_15')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_16">{{Lang::get('content.faq2_16')}}</dt>
+                    <dd>{{Lang::get('content.ans2_16')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_17">{{Lang::get('content.faq2_17')}}</dt>
+                    <dd>{{Lang::get('content.ans2_17')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_18">{{Lang::get('content.faq2_18')}}</dt>
+                    <dd>{{Lang::get('content.ans2_18')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_19">{{Lang::get('content.faq2_19')}}</dt>
+                    <dd>{{Lang::get('content.ans2_19')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_20">{{Lang::get('content.faq2_20')}}</dt>
+                    <dd>{{Lang::get('content.ans2_20')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_21">{{Lang::get('content.faq2_21')}}</dt>
+                    <dd>{{Lang::get('content.ans2_21')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_22">{{Lang::get('content.faq2_22')}}</dt>
+                    <dd>{{Lang::get('content.ans2_22')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_23">{{Lang::get('content.faq2_23')}}</dt>
+                    <dd>{{Lang::get('content.ans2_23')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_24">{{Lang::get('content.faq2_24')}}</dt>
+                    <dd>{{Lang::get('content.ans2_24')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_25">{{Lang::get('content.faq2_25')}}</dt>
+                    <dd>{{Lang::get('content.ans2_25')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_26">{{Lang::get('content.faq2_26')}}</dt>
+                    <dd>{{Lang::get('content.ans2_26')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_27">{{Lang::get('content.faq2_27')}}</dt>
+                    <dd>{{Lang::get('content.ans2_27')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_28">{{Lang::get('content.faq2_28')}}</dt>
+                    <dd>{{Lang::get('content.ans2_28')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_29">{{Lang::get('content.faq2_29')}}</dt>
+                    <dd>{{Lang::get('content.ans2_29')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_30">{{Lang::get('content.faq2_30')}}</dt>
+                    <dd>{{Lang::get('content.ans2_30')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_31">{{Lang::get('content.faq2_31')}}</dt>
+                    <dd>{{Lang::get('content.ans2_31')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_32">{{Lang::get('content.faq2_32')}}</dt>
+                    <dd>{{Lang::get('content.ans2_32')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_33">{{Lang::get('content.faq2_33')}}</dt>
+                    <dd>{{Lang::get('content.ans2_33')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_34">{{Lang::get('content.faq2_34')}}</dt>
+                    <dd>{{Lang::get('content.ans2_34')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_35">{{Lang::get('content.faq2_35')}}</dt>
+                    <dd>{{Lang::get('content.ans2_35')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_36">{{Lang::get('content.faq2_36')}}</dt>
+                    <dd>{{Lang::get('content.ans2_36')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_37">{{Lang::get('content.faq2_37')}}</dt>
+                    <dd>{{Lang::get('content.ans2_37')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_38">{{Lang::get('content.faq2_38')}}</dt>
+                    <dd>{{Lang::get('content.ans2_38')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_39">{{Lang::get('content.faq2_39')}}</dt>
+                    <dd>{{Lang::get('content.ans2_39')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_40">{{Lang::get('content.faq2_40')}}</dt>
+                    <dd>{{Lang::get('content.ans2_40')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_41">{{Lang::get('content.faq2_41')}}</dt>
+                    <dd>{{Lang::get('content.ans2_41')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_42">{{Lang::get('content.faq2_42')}}</dt>
+                    <dd>{{Lang::get('content.ans2_42')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_43">{{Lang::get('content.faq2_43')}}</dt>
+                    <dd>{{Lang::get('content.ans2_43')}}</dd>
+                </li>
+                <li>
+                    <dt id="2_44">{{Lang::get('content.faq2_44')}}</dt>
+                    <dd>{{Lang::get('content.ans2_44')}}</dd>
+                </li>
+            </ol>
+        </dl>
+
     </div>
 
+<a href="#pagetop" class="pagetop"><img src="img/icon_gototop.png"/></a>
+
 </div><!--/row-fluid-->
+
+@include('include.modal_terms_and_conditions')
+
 @stop
+
 
 @section('scripts')
 @parent
+{{ HTML::script('js/jquery.scrollTo.min.js') }}
+{{ HTML::script('js/jquery.localscroll.min.js') }}
+<script type="text/javascript">
+    jQuery(document).ready(function () {
+        jQuery.localScroll();
+    });
+</script>
+
 @stop
+
+
+
